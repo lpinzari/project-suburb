271f8819b8c5d7c582aaf0696c36a5bd32947657
freddie <freddiebutcher@gmail.com>
2019-08-17 17:21:13 +1000

    features table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_013951) do
+ActiveRecord::Schema.define(version: 2019_08_17_063434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "features", force: :cascade do |t|
+    t.text "suburb_id"
+    t.text "suburb_description"
+    t.text "suburb_image_1"
+    t.text "suburb_image_2"
+    t.text "suburb_image_3"
+    t.text "suburb_image_4"
+    t.text "suburb_image_5"
+    t.text "suburb_image_6"
+    t.text "lifestyle_description"
+    t.text "lifestyle_image"
+    t.text "living_cost_description"
+    t.integer "family_cost"
+    t.integer "couple_cost"
+    t.integer "single_cost"
+    t.text "renting_description"
+    t.integer "apartment_rent"
+    t.integer "apartment_availability"
+    t.integer "town_rent"
+    t.integer "town_availability"
+    t.integer "suburban_rent"
+    t.integer "suburban_availability"
+    t.text "entertainment_description"
+    t.text "entertainment_image"
+    t.text "transport_description"
+    t.text "train_lines"
+    t.text "train_description"
+    t.text "bus_routes"
+    t.text "bus_description"
+    t.text "amenities_description"
+  end
 
   create_table "suburbs", force: :cascade do |t|
     t.string "name"
